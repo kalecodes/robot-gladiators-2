210c30488fac475e25cfc94ac22881e585b580cd
kalen510 <kalen.wiley@gmail.com>
2023-03-29 22:25:27 -0400

    first function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// this is a function declaration (uses the function key word)
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
