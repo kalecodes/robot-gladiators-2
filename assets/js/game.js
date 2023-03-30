9d9a3b818da1a19bf12437cdfceffb4d637a4517
kalen510 <kalen.wiley@gmail.com>
2023-03-30 11:27:03 -0400

    add initial round of fight sequence

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,35 @@
 
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 // this is a function declaration (uses the function key word)
-function fight() {
-    window.alert("The fight has begun!");
-}
+// function fight() {
+//     window.alert("Welcome to Robot  Gladiators");
+// };
+// this is the same function as an expression
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-// fight();
+    // Player attack and update enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+    // Log result
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // Enemy attack and update playetHealth
+    playerHealth = playerHealth - enemyAttack;
+    // Log result
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
